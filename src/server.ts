28928857c943e55b92e67b5ca7e5d223774e63d7
sangpham27 <bsangb@outlook.com>
2023-03-10 14:19:34 +0700

    Update package.json file and Elastic Beantalk success screenshot

--- src/server.ts
+++ src/server.ts
@@ -31,13 +31,13 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   app.get('/filteredimage', async (req: Request, res: Response) => {
     const image_url = req.query.image_url.toString();
     if (!image_url) {
-        res.status(400).send('image url is required')
+        res.status(400).send('image url is required');
     }
 
     const filteredImage = await filterImageFromURL(image_url)
 
     res.status(200).sendFile(filteredImage, () => {
-      deleteLocalFiles([filteredImage])
+      deleteLocalFiles([filteredImage]);
     })
   })
 
